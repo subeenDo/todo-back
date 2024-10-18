4731c9c7c7a642dfe868c44320baab37eb0d9f39
subeen <soobin2318@gmail.com>
2024-10-18 16:57:36 +0900

    deploy env

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require('dotenv').config()
 
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todoApp`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
     .connect(mongoURI)
