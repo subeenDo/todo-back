b845eb0815bd91379c9cb90466ef117550bcfe65
subeen <soobin2318@gmail.com>
2024-10-18 17:50:25 +0900

    deplay err2

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 require('dotenv').config()
 
-
 const app = express();
-const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
 const mongoURI = MONGODB_URI_PROD;
 
+const PORT = process.env.PORT || 4000; 
+
 mongoose
     .connect(mongoURI)
     .then(() => {
         console.log("mongoose connected");
+        app.listen(PORT, () => {
+            console.log(`server on ${PORT}`);
+        });
     })
     .catch((err) => {
         console.log("DB connection fail", err);
     });
 
-app.listen(4000, () => {
-    console.log("server on 4000");
-});
+// app.listen(4000, () => {
+//     console.log("server on 4000");
+// });
+
 
 
 // const express = require("express")
